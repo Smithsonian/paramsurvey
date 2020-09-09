fde797490590e07963c2587f6ff88744fb673e93
Greg Lindahl <lindahl@pbm.com>
2020-09-08 23:44:51 -0700

    osx support

--- test/integration/test_generic.py
+++ test/integration/test_generic.py
@@ -4,6 +4,7 @@ import tempfile
 import pytest
 import sys
 from io import StringIO
+import platform
 
 import pandas as pd
 
@@ -120,10 +121,15 @@ def do_test_args(pset, system_kwargs, user_kwargs):
 
 
 def test_args(capsys, paramsurvey_init):
-    chdir = tempfile.gettempdir()
+    if platform.system() == 'Darwin':
+        # in Darwin, the tempfile directory is process-specific
+        # and can't be accessed by a different multiprocessing Pool process
+        # this one is shared between all processes
+        chdir = '/private/var/tmp'
+    else:
+        chdir = tempfile.gettempdir()
     if os.getcwd() == chdir:
-        # whoops
-        pass
+        pytest.skip('somehow we were already in the chdir')
 
     out_func_called = False
     test_user_kwargs = {'test': 1, 'expected_cwd': chdir}
