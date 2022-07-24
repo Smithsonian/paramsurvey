3cebd13b4773537f41dd3b3330da0b5556d3285b
Greg Lindahl <lindahl@pbm.com>
2022-07-23 17:14:56 -0700

    tweaks

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         #'Programming Language :: Python :: 3.5',  # ray no longer supports py3.5, also setuptools_scm problem
-        'Programming Language :: Python :: 3.6',
+        #'Programming Language :: Python :: 3.6',  # setuptools_scm dropped in v7
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
